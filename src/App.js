3cd13c3e35808e2556c1387e54371eb12f4eb49e
elifdiril <elifdiril6@gmail.com>
2022-04-21 11:48:45 +0300

    stars and photos array added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import SelectableAttributes from "./components/selectableAttribute";
 import Stars from "./components/stars";
 import Title from "./components/title";
 import productData from "./assets/product-data.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BaremList from "./components/baremList";
 import BasketButton from "./components/basketButton";
 import QuantityInput from "./components/quantityInput";
+import "./App.css";
 
 function App() {
   const [selectedFirstAttribute, setSelectedFirstAttribute] = useState(
     productData.selectableAttributes[0]
   );
   const [selectedSecondAttribute, setSelectedSecondAttribute] = useState();
+  const [photos, setPhotos] = useState();
+
+  useEffect(()=>{
+    setPhotos(productData.productVariants.map(function(item) {return item["images"]}))
+  },[selectedFirstAttribute, selectedSecondAttribute])
 
   return (
     <div className="product">
+      <div className="photos" >
+        gfhfgh
+      </div>
+      <div className="product-details">
       <Title title={productData?.productTitle} />
       <Stars />
       {productData?.selectableAttributes.map((item, id) => (
@@ -23,6 +33,7 @@ function App() {
       <BaremList list={productData?.baremList} />
       <QuantityInput/>
       <BasketButton/>
+      </div>
     </div>
   );
 }
--- src/components/stars/index.js
+++ src/components/stars/index.js
@@ -2,9 +2,14 @@ import styles from"./styles.module.css";
 
 function Stars(props) {
     return (
-        <div className={styles.stars}>
-            ****
-        </div>
+        <div>
+      {[...Array(4)].map((star, id) => {        
+        return (         
+          <span key={id} className={styles.stars}>&#9733;</span>        
+        );
+      })} 
+      23 Yorum
+    </div> 
     );
 }
 
